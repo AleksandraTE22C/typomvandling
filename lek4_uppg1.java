2a481c413e1d666df14c505b2790c3f6adf53e74
AleksandraTE22C <aleksandra.kajak-tonkiel@elev.ga.ntig.se>
2023-09-23 15:40:00 +0200

    Klar med uppgift A

---
+++ lek4_uppg1.java
@@ -0,0 +1,12 @@
+public class lek4_uppg1{
+    public static void main(String[] args){
+
+        int tal1 = 4;
+        int tal2 = 5;
+        double kvot = (double)tal1/tal2;
+
+        System.out.println("Uppgift A");
+        System.out.println(tal1+" / "+tal2+" = "+kvot);  
+        System.out.println("som heltal: "+tal1+" / "+tal2+" = "+(int)kvot);
+    }
+}
